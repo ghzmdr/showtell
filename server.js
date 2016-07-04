90de19a47c0e1ff7acaf881bac84d38ec2877d67
Vincent <vincent@rollstudio.it>
2016-07-04 11:39:18 +0200

    Changes for RHC

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 var express = require('express')
 var handlebars = require('handlebars')
 var fs = require('fs')
@@ -72,4 +75,4 @@ function findSlideByField(field, match) {
     return slide
 }
 
-app.listen(5000)
+app.listen(server_port, server_ip_address)
